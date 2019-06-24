65ff5f302927c6a4e2299edbd4f7b0c927fecc6a
Atul Malakar <atulmalakar15@gmail.com>
2019-06-24 19:35:26 +0530

    Create SlidingWindow(k - size max subarray).cpp

---
+++ SlidingWindow(k - size max subarray).cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{	
+	int n,k;
+	cin>>n>>k;
+
+	vector<int> v(n);
+
+	for(int i=0;i<n;i++)
+		cin>>v[i];
+	long long int prefixsum=0;
+	for(int i=0;i<k;i++)
+		prefixsum+=v[i];
+	long long int maxsum = prefixsum;
+	int j=0;
+	for(int i=k;i<n;i++)
+	{	
+		prefixsum += (v[i] - v[i-k]);
+		maxsum = max(maxsum,prefixsum);
+	}
+	cout<<maxsum;
+
+}
